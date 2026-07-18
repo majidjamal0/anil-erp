c2c143e867fa68a50209bafb8c2d1843bb21cf05
majidjamal0 <majid.jamal0@gmail.com>
2026-07-18 08:44:35 +0330

    style: format Sprint 2 frontend shell

--- frontend/src/layouts/DashboardLayout.vue
+++ frontend/src/layouts/DashboardLayout.vue
@@ -21,21 +21,26 @@ async function logout() {
         <RouterLink to="/">داشبورد</RouterLink>
         <RouterLink v-if="auth.can('users.view')" to="/users">کاربران</RouterLink>
         <RouterLink v-if="auth.can('roles.manage')" to="/roles">نقش‌ها</RouterLink>
-        <RouterLink v-if="auth.can('permissions.manage')" to="/permissions">
-          مجوزها
-        </RouterLink>
+        <RouterLink v-if="auth.can('permissions.manage')" to="/permissions"> مجوزها </RouterLink>
       </nav>
     </aside>
 
     <div class="content">
       <header class="topbar">
-        <button class="menu" type="button" aria-label="باز کردن منو" @click="sidebarOpen = !sidebarOpen">
+        <button
+          class="menu"
+          type="button"
+          aria-label="باز کردن منو"
+          @click="sidebarOpen = !sidebarOpen"
+        >
           ☰
         </button>
         <span>{{ auth.user?.name }}</span>
         <button type="button" @click="logout">خروج</button>
       </header>
-      <main><RouterView /></main>
+      <main>
+        <RouterView />
+      </main>
     </div>
 
     <button
